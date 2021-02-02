3bcef43b7e46e0ffb3314db6a60277e9b0637bc4
Maksim Korzh <freesoft.for.people@gmail.com>
2021-02-02 13:12:42 +0200

    implemented puzzle solver

---
+++ apps/puzzle_solver/gui/game/puzzles.js
@@ -0,0 +1,20 @@
+const Puzzles = [
+  {
+    id: 'puzzle_0',
+    title: 'bk 2 move kills 01',
+    fen: '4k1r2/1R7/3R3nr/2p1N4/n8/9/9/5p3/c1c1p1p2/5K3 w - - 0 1',
+    description: 'http://www.xqinenglish.com/index.php/en/xiangqi-puzzles/209-dailypuzzles02movers/319-daily-puzzles-02-movers-01'
+  },
+  {
+    id: 'puzzle_1',
+    title: 'bk 2 move kills 02',
+    fen: '5k3/9/2c2N2b/3N1RrR1/2b1n4/9/9/6cC1/2r1p4/3K5 w - - 0 1',
+    description: 'http://www.xqinenglish.com/index.php/en/xiangqi-puzzles/209-dailypuzzles02movers/319-daily-puzzles-02-movers-01'
+  },
+  {
+    id: 'puzzle_2',
+    title: 'bk 2 move kills 03',
+    fen: '2r2k3/8C/b4N1cb/6R2/5h3/6C3/7n2/6p3/4p4/5K1R1 w - - 0 1',
+    description: 'http://www.xqinenglish.com/index.php/en/xiangqi-puzzles/209-dailypuzzles02movers/319-daily-puzzles-02-movers-01'
+  }
+]
